5ea1fd9fb237e5fd0a475eec7aebc5f2a8c5e332
Vilém Charwot <mrlogen1299@gmail.com>
2024-03-26 14:01:30 +0100

    Projects created

---
+++ RoomSense.Api.Lib.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace RoomSense.Api.Lib.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ RoomSense.Api.Lib/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace RoomSense.Api.Lib;
+
+public class Class1
+{
+}
