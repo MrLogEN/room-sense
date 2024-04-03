beb263c141a5436692f12a595150c74a3ab2f3d8
Vilém Charwot <mrlogen1299@gmail.com>
2024-04-03 21:55:32 +0200

    ApiKeyFilter.cs created

---
+++ RoomSense.Api.Lib/Authentication/ApiKeyFilter.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+
+namespace RoomSense.Api.Lib.Authentication;
+
+public class ApiKeyFilter : IAsyncAuthorizationFilter
+{
+    private readonly IConfiguration _configuration;
+
+    public ApiKeyFilter(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        if (context.HttpContext.Request.Headers.TryGetValue(
+                ApiAuthConstants.ApiKeyHeaderName,
+                out var extractedApiKey))
+        {
+            context.Result = new UnauthorizedObjectResult("API Key is missing.");
+            return;
+        }
+
+        var apiKey = _configuration.GetValue<string>(ApiAuthConstants.SectionName);
+
+        if (apiKey is null)
+        {
+            throw new ArgumentNullException(nameof(apiKey), "API key was not found");
+        }
+        
+        if (!apiKey.Equals(extractedApiKey))
+        {
+            context.Result = new UnauthorizedObjectResult("API Key is invalid.");
+            return;
+        }
+    }
+}
