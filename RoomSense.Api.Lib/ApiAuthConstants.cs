70d76c1c6c1b4324318a85a2f01cad5d3e7c998e
Vilém Charwot <mrlogen1299@gmail.com>
2024-04-03 21:20:25 +0200

    Class with constants for authentication.

---
+++ RoomSense.Api.Lib/ApiAuthConstants.cs
@@ -0,0 +1,7 @@
+namespace RoomSense.Api.Lib;
+
+public static class ApiAuthConstants
+{
+    public static string SectionName = "Authentication:ApiKey";
+    public static string ApiKeyHeaderName = "X-Api-Key";
+}
