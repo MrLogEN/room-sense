5106706172e6bde8879451a25f29af7121ca39ef
Vilém Charwot <mrlogen1299@gmail.com>
2024-04-05 17:37:30 +0200

    Renamed columns and set their data types

--- RoomSense.Api.Lib/Data/TemperatureHumidityModelConfiguration.cs
+++ RoomSense.Api.Lib/Data/TemperatureHumidityModelConfiguration.cs
@@ -9,6 +9,34 @@ public class TemperatureHumidityModelConfiguration :
 {
     public void Configure(EntityTypeBuilder<TemperatureHumidity> builder)
     {
-        throw new NotImplementedException();
+        builder
+            .ToTable("temphum")
+            .HasKey(t => t.Id);
+
+        builder
+            .Property(t => t.Id)
+            .HasColumnType("char(36)")
+            .IsRequired()
+            .HasColumnName("id");
+
+        builder
+            .Property(t => t.TimeStamp)
+            .IsRequired()
+            .HasColumnType("timestamp")
+            .HasColumnName("time");
+
+        builder
+            .Property(t => t.Humidity)
+            .IsRequired()
+            .HasColumnName("hum");
+
+        builder
+            .Property(t => t.Temperature)
+            .IsRequired()
+            .HasColumnName("temp");
+
+        builder
+            .Property(t => t.Cluster)
+            .HasColumnName("cluster");
     }
 }
