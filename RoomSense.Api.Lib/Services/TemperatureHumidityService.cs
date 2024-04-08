3e28c03c12fa1cfa846bf93739c5c13c62261cde
Vilém Charwot <mrlogen1299@gmail.com>
2024-04-08 20:28:22 +0200

    GetAllRecords method prototype implementation, needs changes

--- RoomSense.Api.Lib/Services/TemperatureHumidityService.cs
+++ RoomSense.Api.Lib/Services/TemperatureHumidityService.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RoomSense.Api.Lib.Data;
 using RoomSense.Api.Lib.Data.DTOs;
+using RoomSense.Api.Lib.Models;
 
 namespace RoomSense.Api.Lib.Services;
 
@@ -15,8 +16,20 @@ public class TemperatureHumidityService
         _context = context;
     }
 
-    public void CreateRecord(CreateTemperatureHumidity record)
+    public async Task CreateRecord(CreateTemperatureHumidity record)
     {
         
+    }
+
+    public async Task<IEnumerable<GetAllRecords>> GetAllRecords()
+    {
+        return await _context.TemperaturesAndHumidities
+            .Select(t => new GetAllRecords()
+            {
+                ClusterName = t.Cluster.Name,
+                Humidity = t.Humidity,
+                Temperature = t.Temperature,
+                TimeStamp = t.TimeStamp
+            }).ToListAsync();
     }
 }
